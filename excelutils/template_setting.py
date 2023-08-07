d15042a3420ce6115ff32020adc68498bf00cab4
Konstantin Kazak <e2e4suchok@gmail.com>
2023-08-07 19:52:32 +0300

    tamplate add

--- excelutils/template_setting.py
+++ excelutils/template_setting.py
@@ -22,7 +22,7 @@ headers = {
 width_columns = {'A': 8, 'B': 10, 'C': 12, 'E': 15, 'F': 10, 'G': 80, 'H': 15, 'I': 16, 'J': 10, 'K': 15}
 
 
-colors_styling = {'lavender': 'E6E6FA', 'thistle': '00D8BFD8', 'title_basic': "00FAFAE6", 'c1': "0099CCFF", 'c2': "00FF9900", 'c3': "0099CC00", 'c4': "00FFCC00", 'c5': "000066CC", 'c6': "00666699", 'c7': "00C0C0C0", 'c8': "00FF99CC"}
+colors_styling = {'lavender': 'E6E6FA', 'thistle': '00D8BFD8', 'title_basic': "00FAFAE6", 'title_attributes': "00daeef3", 'title_parameter': "00fde9d9", 'c3': "0099CC00", 'c4': "00FFCC00", 'c5': "000066CC", 'c6': "00666699", 'c7': "00C0C0C0", 'c8': "00FF99CC"}
 
 formats = [
     {
@@ -32,10 +32,25 @@ formats = [
         'fill':       {'patternType': "solid", 'fgColor': colors_styling['title_basic']},
         'border':     {'style': 'thin', 'color': "000000"}
     },
+    {
+        'name':       'title_parameter',
+        'font':       {'name': 'Calibri', 'bold': False, 'size': 10},
+        'alignment':  {'horizontal': 'center', 'vertical': 'bottom', 'wrap_text': True, 'shrink_to_fit': False, 'indent': 0},
+        'fill':       {'patternType': "solid", 'fgColor': colors_styling['title_parameter']},
+        'border':     {'style': 'thin', 'color': "000000"}
+    },
+{
+        'name':       'title_attributes',
+        'font':       {'name': 'Calibri', 'bold': False, 'size': 10},
+        'alignment':  {'horizontal': 'center', 'vertical': 'bottom', 'wrap_text': True, 'shrink_to_fit': False, 'indent': 0},
+        'fill':       {'patternType': "solid", 'fgColor': colors_styling['title_attributes']},
+        'border':     {'style': 'thin', 'color': "000000"}
+    },
+
+
+
+
 ]
-
-# formats = [{1:"aaaa", 2:'bbb'}, {55:'rerrr', 88:'yyyy'}]
-
 
 #
 #
